490d9dbb7d30212e84d818be071f76f30074d29a
Hasret Karci <83207900+KintheN-git@users.noreply.github.com>
2025-02-12 22:08:49 +0300

    file actions update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,7 @@ import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
-
+import { Toaster } from "@/components/ui/toaster";
 export const dynamic = "force-dynamic";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
@@ -18,9 +18,10 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
 
       <section className="flex h-full flex-1 flex-col">
         <MobileNavigation {...currentUser} />
-        <Header />
+        <Header accountId={currentUser.accountId} userId={currentUser.$id} />
         <div className="main-content">{children}</div>
       </section>
+      <Toaster />
     </main>
   );
 };
